6001d26775cd02c22d08a5439207f3356e851d5f
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-06-17 16:45:05 +0600

    slider-problem-fixed

--- src/components/ui/HomePage/HeroSection/index.tsx
+++ src/components/ui/HomePage/HeroSection/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
 import "./HeroSection.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import slider from "../../../../assets/images/slider2.webp";
 import slider2 from "../../../../assets/images/categories.png";
 import slider3 from "../../../../assets/images/flash8.png";
@@ -13,8 +13,11 @@ import slider8 from "../../../../assets/images/slider5.jpg";
 import slider9 from "../../../../assets/images/slider6.webp";
 import slider10 from "../../../../assets/images/headphone.png";
 
-import { KeenSliderInstance, useKeenSlider } from "keen-slider/react";
-import "keen-slider/keen-slider.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import Image from "next/image";
 import {
   FaBicycle,
@@ -24,44 +27,16 @@ import {
   FaTshirt,
   FaUtensils,
 } from "react-icons/fa";
+import  { Autoplay }  from "swiper/modules";
 import { HiDeviceTablet, HiOutlineWifi } from "react-icons/hi";
 import { LuWatch, LuYoutube } from "react-icons/lu";
 import { HiOutlineComputerDesktop } from "react-icons/hi2";
 import Container from "@/components/shared/Container";
 import { Button } from "@mui/material";
-
-
 export default function HeroSection() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [loaded, setLoaded] = useState(false);
-
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    slideChanged(slider) {
-      const keenSlider = slider as KeenSliderInstance<any, any, any>;
-      if (keenSlider.details) {
-        setCurrentSlide(keenSlider.details().relativeSlide as number);
-      }
-      // setCurrentSlide(slider.details().relativeSlide as number);
-    },
-    created() {
-      setLoaded(true);
-    },
-    loop: true, // Enable looping
-  });
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (instanceRef.current) {
-        instanceRef.current.next();
-      }
-    }, 5000); // Change the interval duration as needed (2000 ms = 2 seconds)
-
-    return () => clearInterval(interval);
-  }, [instanceRef]);
   return (
     <Container className="heroSectionContainer">
-      <div className="heroSectionWraps ">
+      <div className="heroSectionWraps">
         <div className="categoryHead space-y-4">
           <div className="flex items-center">
             <FaPercent className="mr-2" size={25} />
@@ -85,7 +60,7 @@ export default function HeroSection() {
           </div>
           <div className="flex items-center">
             <FaUtensils className="mr-2" size={25} />
-            <span> Home Applicanes </span>
+            <span> Home Appliances </span>
           </div>
           <div className="flex items-center">
             <FaTshirt className="mr-2" size={25} />
@@ -117,263 +92,112 @@ export default function HeroSection() {
           </div>
         </div>
         <div className="sliderWrap">
-          <div className="navigation-wrapper">
-            <div ref={sliderRef} className="keen-slider">
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <Button>Start Buying</Button>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            loop={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay]}
+            // pagination={{ clickable: true }}
+            // navigation
+            speed={1500}
+          >
+            <SwiperSlide>
+              <div className="flex items-center sliderContentWrap">
+                <div className="sliderContent">
+                  <h3 className="text-5xl font-semibold uppercase">
+                    The New standard
+                  </h3>
+                  <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
+                  <small>FROM</small>
+                  <div className="flex items-center">
+                    <sup className="text-2xl font-semibold">$</sup>
+                    <span className="text-5xl font-bold">799</span>
+                    <sup className="text-xl font-semibold">99</sup>
                   </div>
-                  <Image src={slider} alt="slider" width="500" height="500" />
+                  <Button>Start Buying</Button>
                 </div>
+                <Image src={slider} alt="slider" width="500" height="500" />
               </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex items-center sliderContentWrap">
+                <div className="sliderContent">
+                  <h3 className="text-5xl font-semibold uppercase">
+                    The New standard
+                  </h3>
+                  <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
+                  <small>FROM</small>
+                  <div className="flex items-center">
+                    <sup className="text-2xl font-semibold">$</sup>
+                    <span className="text-5xl font-bold">799</span>
+                    <sup className="text-xl font-semibold">99</sup>
                   </div>
-                  <Image src={slider2} alt="slider" width="500" height="500" />
+                  <Button>Start Buying</Button>
                 </div>
+                <Image src={slider2} alt="slider" width="500" height="500" />
               </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex items-center sliderContentWrap">
+                <div className="sliderContent">
+                  <h3 className="text-5xl font-semibold uppercase">
+                    The New standard
+                  </h3>
+                  <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
+                  <small>FROM</small>
+                  <div className="flex items-center">
+                    <sup className="text-2xl font-semibold">$</sup>
+                    <span className="text-5xl font-bold">799</span>
+                    <sup className="text-xl font-semibold">99</sup>
                   </div>
-                  <Image src={slider3} alt="slider" width="500" height="500" />
+                  <Button>Start Buying</Button>
                 </div>
+                <Image src={slider3} alt="slider" width="500" height="500" />
               </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex items-center sliderContentWrap">
+                <div className="sliderContent">
+                  <h3 className="text-5xl font-semibold uppercase">
+                    The New standard
+                  </h3>
+                  <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
+                  <small>FROM</small>
+                  <div className="flex items-center">
+                    <sup className="text-2xl font-semibold">$</sup>
+                    <span className="text-5xl font-bold">799</span>
+                    <sup className="text-xl font-semibold">99</sup>
                   </div>
-                  <Image src={slider4} alt="slider" width="500" height="500" />
+                  <Button>Start Buying</Button>
                 </div>
+                <Image src={slider4} alt="slider" width="500" height="500" />
               </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex items-center sliderContentWrap">
+                <div className="sliderContent">
+                  <h3 className="text-5xl font-semibold uppercase">
+                    The New standard
+                  </h3>
+                  <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
+                  <small>FROM</small>
+                  <div className="flex items-center">
+                    <sup className="text-2xl font-semibold">$</sup>
+                    <span className="text-5xl font-bold">799</span>
+                    <sup className="text-xl font-semibold">99</sup>
                   </div>
-                  <Image src={slider6} alt="slider" width="500" height="500" />
+                  <Button>Start Buying</Button>
                 </div>
+                <Image src={slider5} alt="slider" width="500" height="500" />
               </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
-                  </div>
-                  <Image src={slider7} alt="slider" width="500" height="500" />
-                </div>
-              </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
-                  </div>
-                  <Image src={slider8} alt="slider" width="500" height="500" />
-                </div>
-              </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
-                  </div>
-                  <Image src={slider9} alt="slider" width="500" height="500" />
-                </div>
-              </div>
-              <div className="keen-slider__slide number-slide1">
-                <div className="flex items-center sliderContentWrap">
-                  <div className="sliderContent">
-                    <h3 className="text-5xl font-semibold uppercase">
-                      The New standard
-                    </h3>
-                    <p className="my-3">"NeoTech Edge: Unveiling Simplicity"</p>
-                    <small>FROM</small>
-                    <div className=" flex items-center ">
-                      <sup className="text-2xl font-semibold">$</sup>
-                      <span className="text-5xl font-bold">799</span>
-                      <sup className="text-xl font-semibold">99</sup>
-                    </div>
-                    <button className="px-5 py-2 text-white bg-[#2251CF] border rounded-sm text-xl">
-                      {" "}
-                      Start Buying
-                    </button>
-                  </div>
-                  <Image src={slider10} alt="slider" width="500" height="500" />
-                </div>
-              </div>
-            </div>
-            {loaded && instanceRef.current && (
-              <>
-                <Arrow
-                  left
-                  onClick={(e: any) =>
-                    e.stopPropagation() || instanceRef.current?.prev()
-                  }
-                  disabled={currentSlide === 0}
-                />
-
-                <Arrow
-                  onClick={(e: any) =>
-                    e.stopPropagation() || instanceRef.current?.next()
-                  }
-                  disabled={
-                    currentSlide ===
-                    instanceRef.current.track.details.slides.length - 1
-                  }
-                />
-              </>
-            )}
-          </div>
-          {loaded && instanceRef.current && (
-            <div className="dots">
-              {[
-                ...Array(
-                  instanceRef.current.track.details.slides.length
-                ).keys(),
-              ].map((idx) => {
-                return (
-                  <button
-                    key={idx}
-                    onClick={() => {
-                      instanceRef.current?.moveToIdx(idx);
-                    }}
-                    className={"dot" + (currentSlide === idx ? " active" : "")}
-                  ></button>
-                );
-              })}
-            </div>
-          )}
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </Container>
-  );
-}
-
-function Arrow(props: {
-  disabled: boolean;
-  left?: boolean;
-  onClick: (e: any) => void;
-}) {
-  const disabled = props.disabled ? " arrow--disabled" : "";
-  return (
-    <svg
-      onClick={props.onClick}
-      className={`arrow ${
-        props.left ? "arrow--left" : "arrow--right"
-      } ${disabled}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-      )}
-      {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-      )}
-    </svg>
   );
 }
