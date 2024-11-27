43f4cf9cf090425a616ab0b7fb10a870afe31790
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-11-28 01:32:19 +0600

    update

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -19,6 +19,7 @@ const Footer = () => {
               <li>Home Fitness Equipment</li>
               <li>Desk Accessories</li>
               <li>Plant-Based Skincare</li>
+              <li>Update Products </li>
             </ul>
           </div>
           <div>
